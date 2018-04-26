70e328f42247ec85ef847aa8773de093c11fdf00
Brandon Sanchez <brandonsanchez05@gmail.com>
2018-04-25 20:02:27 -0400

    Revert "Updates"
    This reverts commit 5bfca0a729db3f2b4d7998225987a221a722e232.

---
+++ src/components/About/About.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+import './style.css'
+
+export default class About extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return(
+      <div className="about-page" id="About">
+        <div className="content-grid">
+          <h1>About</h1>
+          <div className="about-wrapper">
+            <div className="about-content">
+              <p>This is the about page.</p>
+              <p>Stay tuned, I will eventually write some witty banter that expresses who I am.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
