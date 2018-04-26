c65ced2b97d50b4303933780da857f0d2e70010f
Brandon Sanchez <brandonsanchez05@gmail.com>
2018-04-25 20:50:34 -0400

    Revert "Updates"
    This reverts commit e0722185234ed6d9abf9986c5466d3b892e11b98.

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './HND.svg';
+import Nav from  './components/Nav/Nav';
+import Portfolio from './components/Portfolio/Portfolio'
+import About from './components/About/About'
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <main>
+          <img style={{display: 'block', marginLeft: 'auto', marginRight: 'auto'}} src={logo} height='auto' width='200px'/>
+          <div class="intro">Hi, I'm Brandon!</div>
+      		<div class="tagline">
+            Software Engineer
+            <span style={{color: 'red'}}> | </span>
+             Philosopher
+            <span style={{color: 'red'}}> | </span>
+             Perpetual Learner
+          </div>
+          <div class='icons-social'>
+            <a target="_blank" href="https://github.com/Brandon05"><i class="fab fa-github"></i></a>
+            <a target="_blank" href="https://twitter.com/BrrandonSanchez"><i class="fab fa-twitter"></i></a>
+          </div>
+        </main>
+        <About />
+        <Portfolio />
+      </div>
+    );
+  }
+}
+
+export default App;
