0883e4d67f4e4c13c7d233eb949018a87690161a
Brandon Sanchez <brandonsanchez05@gmail.com>
2018-04-25 21:25:42 -0400

    test

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,6 +14,7 @@ export default class About extends Component {
           <h1>About</h1>
           <div className="about-wrapper">
             <div className="about-content">
+              <p>This is the About page.</p>
               <p>Stay tuned, I will eventually write some witty banter that expresses who I am. Until then, enjoy the empty.</p>
             </div>
           </div>
